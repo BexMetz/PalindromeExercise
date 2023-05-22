ac45a4cea4b68350ea110817cdcaaed3bc53762b
Bex <bexmetzger@gmail.com>
2023-05-22 16:42:54 -0700

    Exercise complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+
+		public bool IsAPalindrome(string word)
+		{
+			char[] wordArray = word.ToCharArray();
+			Array.Reverse(wordArray);
+			var reversed = new string (wordArray);
+			if (reversed == word)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("noon", true)]
+        [InlineData("civic", true)]
+        [InlineData("goodbye", false)]
+        public void PalindromeTest(string word, bool expected)
         {
-
+            var ws = new WordSmith();
+            var actual = ws.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
